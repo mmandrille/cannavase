7c9c6e4cd53d61f30b375865dd85744a76eb4959
Marcos Federico Mandrille <mmandrille@gmail.com>
2019-04-15 13:45:52 -0300

    Se arregla bug en gunicorn

--- cannavase/gunicorn.sh
+++ cannavase/gunicorn.sh
@@ -2,4 +2,4 @@
 cd /opt/cannavase
 source venv/bin/activate
 cd /opt/cannavase/cannavase
-gunicorn cannavase.wsgi -t 600 -b 127.0.0.1:8000 -w 6 --user=servidor --group=servidor --log-file=/opt/cier2019/gunicorn.log 2>>/opt/cier2019/gunicorn.log
+gunicorn cannavase.wsgi -t 600 -b 127.0.0.1:8000 -w 6 --user=servidor --group=servidor --log-file=/opt/cannavase/gunicorn.log 2>>/opt/cannavase/gunicorn.log
